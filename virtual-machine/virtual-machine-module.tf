1b3be5255214e03562a526b2cb3ea081983ae2aa
Arnab <grinit10@gmail.com>
2023-05-22 14:52:23 +1000

    Ignoring tfstate files.

---
+++ virtual-machine/virtual-machine-module.tf
@@ -0,0 +1,17 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                     = var.virtual-network-name
+  resource_group_name      = var.resource-group-name
+  location                 = var.resource-group-location
+  address_space            = ["10.0.0.0/16"]
+  subnet {
+    name           = "default"
+    address_prefix = "10.0.0.0/24"
+  }
+  subnet {
+    name           = "webSubnet"
+    address_prefix = "10.0.1.0/24"
+  }
+  tags = {
+    env = var.environment-value
+  }
+}
---
+++ virtual-machine/virtual-machine-variables.tf
@@ -0,0 +1,15 @@
+variable "resource-group-name" {
+    type = string
+}
+
+variable "resource-group-location" {
+    type = string
+}
+
+variable "environment-value" {
+  type = string
+}
+
+variable "virtual-network-name" {
+    type = string
+}
